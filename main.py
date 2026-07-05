4483e5b67b4d7e92729084c07846214ac7419459
JOKER18sl <96708692+JOKER18sl@users.noreply.github.com>
2026-07-06 00:42:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,21 @@ TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.guilds = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
-
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
-    await bot.tree.sync()
-
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} commands")
+    except Exception as e:
+        print(e)
 
 @bot.tree.command(name="ping", description="Check if bot is online")
 async def ping(interaction: discord.Interaction):
-    await interaction.response.send_message("🏓 Pong!")
-
+    await interaction.response.send_message("🏓 Pong!", ephemeral=True)
 
 bot.run(TOKEN)
